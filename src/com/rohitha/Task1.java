e045778297206d3bf2600e4cb5a4ae2e42342c8e
Rohitha-M-Raghu <rohitha13raghu00@gmail.com>
2023-02-07 14:17:41 +0530

    dummy commit

---
+++ src/com/rohitha/Task1.java
@@ -0,0 +1,31 @@
+package com.rohitha;
+
+import java.util.*;
+
+public class Task1 {
+
+		public static void main(String[] args) {
+			//Mortgage Calculator
+			final byte MONTHS_IN_YEAR = 12;
+			final byte PERCENTAGE = 100;
+			Scanner scanner = new Scanner(System.in);
+			
+			System.out.print("Principal = ");
+			int principle = scanner.nextInt();
+			
+			System.out.print("\nAnnual Interest Rate = ");
+			float annualRate = scanner.nextFloat();
+			float monthlyRate = annualRate / PERCENTAGE / MONTHS_IN_YEAR;
+			
+			System.out.print("\nPeriod(Years) = ");
+			byte years = scanner.nextByte();
+			int numberofPayments = years * MONTHS_IN_YEAR;
+			
+			double morgage = principle * (monthlyRate*Math.pow(1 + monthlyRate, numberofPayments)) /(Math.pow(1 + monthlyRate, numberofPayments) - 1);
+			System.out.print("\nMorgage = " + morgage);
+			
+	}
+}
+
+
+
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author rohitha-pt7161
+ *
+ */
+module Java_Training_Task1 {
+	requires java.desktop;
+}
