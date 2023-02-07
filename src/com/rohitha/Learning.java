d276e3c7c6d4d85f55e75bbb779f0df83a3bf577
Rohitha-M-Raghu <rohitha13raghu00@gmail.com>
2023-02-07 19:49:01 +0530

    Excercise 1 using ArrayList

--- src/com/rohitha/Learning.java
+++ src/com/rohitha/Learning.java
@@ -1,9 +1,43 @@
 package com.rohitha;
+import java.util.*;
+
+class Browser{
+	private static ArrayList<String> history = new ArrayList<String>();
+	
+	public Browser() {	//empty constructor
+	} 
+	
+	public void setBrowser(String history) {	//adding new visited url
+		this.history.add(history);
+	}
+	
+	public Browser(ArrayList<String> urls) {		//constructor with parameter
+		for(int i=0; i< urls.size(); i++) {
+			this.history.add(urls.get(i));
+		}
+	}
+	public void accessBrowser() {		//displaying visited url
+		for(int i = 0; i<history.size(); i++) {
+			System.out.println("[history[" + (i+1) +"]:" + history.get(i));
+		}
+	}
+	
+}
+	
 
 public class Learning {
 
 	public static void main(String[] args) {
-		//Mortgage calculator
+		Browser b1 = new Browser();
+		String url = "https://www.google.com";
+		b1.setBrowser(url);
+		ArrayList<String> urlArray = new ArrayList<String>();
+		urlArray.add("https://www.facebook.com");
+		urlArray.add("https://www.web.whatsapp.com");
+		Browser b2 = new Browser(urlArray);
+		b2.accessBrowser();
+		
+		
 		
 	}
 
