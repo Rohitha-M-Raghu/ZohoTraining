3a45e947373af10b98cf7f2ced31d4624f80f28e
Rohitha-M-Raghu <rohitha13raghu00@gmail.com>
2023-02-10 09:35:20 +0530

    Handling ArrayIndexOutOfBoundsException

--- src/com/rohitha/Dynamic.java
+++ src/com/rohitha/Dynamic.java
@@ -1,28 +1,52 @@
 package com.rohitha;
 
 class Browser{
-	private static String[] history = new String[100];
+	private static String[] history = new String[1];
 	private static int historyIndex = 0;
 	
 	public Browser() {	//empty constructor
 	}
 	
 	public Browser(String[] urls) {	//constructor
-		for(int i=0; i<urls.length; ++i) {
-			Browser.history[Browser.historyIndex] = urls[i];
-			Browser.historyIndex ++;
+			for(int i=0; i<urls.length; ++i) {
+				try {
+				Browser.history[Browser.historyIndex] = urls[i];
+				}
+				catch(Exception e) {
+					Browser.growSize();
+					Browser.history[Browser.historyIndex] = urls[i];
+				}
+				Browser.historyIndex ++;
+			
 		}
+		
 	}
 	
 	public void setBrowser(String url) { //to add new url to history
-		Browser.history[Browser.historyIndex] = url;
-		Browser.historyIndex ++;
+		try {
+			Browser.history[Browser.historyIndex] = url;
+			Browser.historyIndex ++;
+		}
+		catch(Exception e) {
+			Browser.growSize();
+			Browser.history[Browser.historyIndex] = url;
+			Browser.historyIndex ++;
+		}
 	}
 	
 	public void accessBrowser() {	//to display all the browser history
 		for(int i=0; i<Browser.historyIndex; ++i) {
 			System.out.println("[history[" + (i+1) +"]:" + Browser.history[i]);
 		}
+	}
+	private static void growSize() {	//to increase the array size upon exception
+		String[] temp = null;
+		temp = new String[Browser.historyIndex * 2];
+		for(int i=0; i< Browser.historyIndex; ++i) {
+			temp[i] = Browser.history[i];
+		}
+		Browser.history = temp;
+		
 	}
 }
 
